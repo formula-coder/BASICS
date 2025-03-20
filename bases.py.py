ea74d7138215753bdc2252a3b844056f7f078dbc
formula-coder <yezidandres2501@gmail.com>
2025-03-20 10:40:29 -0500

    git add

--- bases.py.py
+++ bases.py.py
@@ -272,3 +272,10 @@ x = 1
 while x <= 10:
     print("ernesto")
     x += 1
+
+"""ejemplo"""
+num_uno, num_dos = 0 , 1
+while num_dos < 1597:
+    print(num_uno, num_dos, end=" " )
+    num_uno = num_uno + num_dos
+    num_dos = num_uno + num_dos
