3ee0f2a29cc9359d35495e234b8e9bc3e16c1cf8
formula-coder <yezidandres2501@gmail.com>
2025-08-05 16:12:39 -0500

    git add

---
+++ Bases 2 c++.cpp
@@ -0,0 +1,28 @@
+//Tipos de datos en c++
+
+#include<iostream>
+#include<stdio.h>
+
+using namesapce std;
+
+int main(){
+	int entero = 15;
+	cout<< entero << " entero "<<;
+}
+float flotante  = 10.45;
+cout<< flotante << " flotante"<<endL;
+
+double mayor  = 3.1415
+cout<< mayor<<" double"<< endl;
+
+printf("%f,"mayor); printf("\n%c","double\n");
+cout.preccision(16);
+cout<< mayor << " double con precision" << endl;
+
+char caracter ='a';
+cout<< caracter<<"caracter"<<endl;
+
+char letras[]= "probando";
+cout<< letras<< "letras"<<endl;
+
+string probando ="probando"
