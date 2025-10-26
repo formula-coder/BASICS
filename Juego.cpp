f0f7ee7b77ea54c5579958cac64db242a124efd3
formula-coder <yezidandres2501@gmail.com>
2025-10-26 17:27:01 -0500

    git add

--- Juego.cpp
+++ Juego.cpp
@@ -22,20 +22,7 @@ int main() {
     int empates[100];
     int perdidos[100];
     int puntos[100];
-
-    // tablero principal inicializado en espacios
     char tablero[6][7];
-   cout << endl;
-    cout << "-----------------------------" << endl;
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 7; j++) {
-            cout << "| " << tablero[i][j] << " ";  
-        }
-        cout << "|"<<endl;
-    }
-    cout << "-----------------------------" << endl;
-    cout << "  1   2   3   4   5   6   7 " << endl << endl;
-
     cout << "Juego de cuatro en linea" << endl;
     cout << "-----------------------" << endl;
 
