05d2cbef79c001300a7886d096489e06411f7643
Yezid Andres Guzman Periñan <yezidandres2501@gmail.com>
2026-01-28 08:53:27 -0500

    trabajo en clase

---
+++ Semestre 2/Ejercicios base.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+struct persona{
+    string nombre;
+    string numero;
+};
+int  main(){
+cout<<"Programa de agenda telefonica"<<endl;
+cout<<"------------------------------"<<endl;
+cout <<"Menu de opciones"<<endl;
+cout <<"1. Agregar contactos"<<endl;
+cout <<"2. Mostrar contactos"<<endl;
+cout<<"3.Editar contactos"<<endl;
+cout<<"4.Eliminar contactos"<<endl;
+cout<<"5.Salir"<<endl;
+return 0;
+}
