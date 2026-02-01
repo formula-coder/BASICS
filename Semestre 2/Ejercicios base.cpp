4d721d8257e66c71c1a8d73c1cf5cc7ea6d90284
formula-coder <yezidandres2501@gmail.com>
2026-01-31 22:01:25 -0500

    git add

--- Semestre 2/Ejercicios base.cpp
+++ Semestre 2/Ejercicios base.cpp
@@ -6,8 +6,8 @@ int  main(){
 cout<<"Programa de agenda telefonica"<<endl;
 cout<<"------------------------------"<<endl;
 cout <<"Menu de opciones"<<endl;
-cout <<"1. Agregar contactos"<<endl;
-cout <<"2. Mostrar contactos"<<endl;
+cout <<"1.Agregar contactos"<<endl;
+cout <<"2.Mostrar contactos"<<endl;
 cout<<"3.Editar contactos"<<endl;
 cout<<"4.Eliminar contactos"<<endl;
 cout<<"5.Salir"<<endl;
