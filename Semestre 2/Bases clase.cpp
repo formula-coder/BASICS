c5931c3fe2e4b29478e620cb19e21bfbfbd187f5
formula-coder <yezidandres2501@gmail.com>
2026-02-02 08:47:16 -0500

    git add

--- Semestre 2/Bases clase.cpp
+++ Semestre 2/Bases clase.cpp
@@ -65,3 +65,86 @@ int main() {
 }
 
 //clase 2 :
+//Operadores: Al crear estructuras de datos nuevas no existe una definicion para los operadores basicos, por lo que es necesario definir un proceso para cada uno de ellos.
+
+struct contact{
+    string name;
+    string phone;
+};
+
+int main(){
+    contact c;
+    cout<<"Nombre: "; cin>>c.name;
+    cout<<"Telefono: "; cin>>c.phone;
+    cout<<"Contacto guardado: "<<c.name<<", "<<c.phone<<endl;
+    return 0;
+}
+
+//Operadores de asignacion: Se define el comportamiento del operador como una funcion dentro de la estructura con el retorno esperado depues de aplicar el operador en esta.
+
+struct contact{
+    string name;
+    string phone;
+    //constructor
+    contact(){
+        this->name;
+        this->phone;
+    }
+    friend ostream& operator<<(ostream& os, const contact& c){
+        os << "Nombre: " << c.name << ", Telefono: " << c.phone;
+        return os;
+    }
+};
+//Escritura: 1. Libreria fstream 2. declaracion de la variable 3. validar archivo 4. operador de escritura(<<) 5. cerrar archivo.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    fstream archivo("contactos.txt", ios::out);
+    int valor =100;
+    if(archivo.is_open()){
+        archivo << "Valor: " << valor << endl;
+        archivo.close();
+
+}
+return 0;
+}
+
+//Lectura de archivos: 1. Libreria fstream 2. declaracion de la variable 3. validar archivo 4. operador de lectura(>>) 5. cerrar archivo.
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    fstream archivo ("archivo.txt", ios::in);
+    int valor;
+    if(archivo){
+        archivo >> valor;
+        cout<<"Valor leido: "<<valor<<endl;
+        archivo.close();
+    }
+    return 0;
+}
+
+// archivos binarios
+// Formato de archivo basado en bits para almacenar de manera eficiente valores, almacenandolo de la manera exacta en la que se encuentran en memoria.
+//Extensiones: .bin, .dat, .jpg, .png, .mp3, .mp4
+
+//Escritura en archivos binarios: Se alamcenan los elementos en memoria con la funcion write(), se recibe la posicion en memoria del valor a guardar y el tamaño en bytes del valor.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    fstream archivo("archivo.bin",ios::out | ios::binary);
+    int valor=12345;
+    if(archivo){
+        archivo.write(reinterpret_cast<char*>(&valor), sizeof(valor));
+        archivo.close();
+    }
+}
