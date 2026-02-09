4ea785368ec37a0a679ed4ffbee33762cff108c7
formula-coder <yezidandres2501@gmail.com>
2026-02-09 18:09:32 -0500

    git add

--- Semestre 2/Ordenamiento.cpp
+++ Semestre 2/Ordenamiento.cpp
@@ -1,1 +1,54 @@
-//Ejercicio:Construir un sistema el cual,a partir de un arreglo de tamaño10 creado con numero aleatorios entre el 1  y el 100, cree un arreglo auxiliar con sus psoiciones en memoria y realice un ordenamiento sobre este sin alterar el arreglo original.  Es obligatorio el uso de aritmetica de apuntadores.
+#include <iostream>
+#include <time.h>
+#include <conio.h>
+
+using namespace std;
+
+
+void ordenar (int **arrPunteros){
+
+    for (int i = 0; i < 9; i++) {
+        int imin = i;
+        for (int j = i + 1; j < 10; j++) {
+            if (**(arrPunteros + j) < **(arrPunteros + imin)) {
+                imin = j;
+            }
+        }
+        if (imin != i) {
+            int *aux = *(arrPunteros + imin);
+            *(arrPunteros + imin) = *(arrPunteros + i);
+            *(arrPunteros + i) = aux;
+        }
+    }
+
+}
+
+int main(){
+
+
+    srand(time(NULL));
+
+    int * arrOriginal = new int [10];
+
+    cout<<"Arreglo original"<<endl;
+    for(int i=0; i<10;i++){
+       *(arrOriginal + i)= rand() %100 +1;
+        cout<<*(arrOriginal + i)<<" | ";
+    }
+    cout<<endl;
+   int **arrPunteros = new int* [10];
+
+   for(int i=0; i<10; i++){
+
+      *(arrPunteros + i)= arrOriginal + i;
+
+   }
+cout<<"Arreglo ordenado"<<endl;
+cout<<"\n";
+    ordenar(arrPunteros);
+    for(int i=0; i<10;i++){
+        cout<<**(arrPunteros + i)<<" | ";
+    }
+
+getch();
+}
