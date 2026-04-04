4aa1cd062fc4acb6cd1f2c1dfe1cbba68e961b57
formula-coder <yezidandres2501@gmail.com>
2026-04-04 18:46:04 -0500

    git add

--- Proyecto del juego/main.cpp
+++ Proyecto del juego/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char* argv[]) {
         }
     }
     
-    if (comandoInicialPendiente == "start") {
+    if (comandoInicialPendiente == "inicio") {
         juego.iniciar(10, 5, 3, 2, 1); // valores por defecto
         juegoIniciado = true;
     }
