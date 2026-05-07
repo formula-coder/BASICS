6b358d6bc6e823ac2f387d1f3d87225289f4b7d6
formula-coder <yezidandres2501@gmail.com>
2026-05-06 20:50:41 -0500

    git add

--- Semestre 2 Java/Basesjava.java
+++ Semestre 2 Java/Basesjava.java
@@ -7,3 +7,6 @@ public class Basesjava{
 //tipos de datos: Enteros byte short int long
 //decimales float double
 //otros char boolean String
+
+//librerias: java.util.Scanner
+//sirve para leer datos por consola, se importa al inicio del codigo, se crea un objeto de la clase Scanner y se utiliza el metodo nextLine() para leer una linea de texto, nextInt() para leer un entero, nextDouble() para leer un decimal, etc.
---
+++ Semestre 2 Java/basesjavaejercicios.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class basesjavaejercicios {
+    public static void main(String args[]){
+
+    }
+}
+
