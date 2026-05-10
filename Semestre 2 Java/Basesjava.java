7021860ab92a3bc66499496ff216f25b02f8f63c
formula-coder <yezidandres2501@gmail.com>
2026-05-10 18:16:53 -0500

    git add

--- Semestre 2 Java/Basesjava.java
+++ Semestre 2 Java/Basesjava.java
@@ -10,3 +10,50 @@ public class Basesjava{
 
 //librerias: java.util.Scanner
 //sirve para leer datos por consola, se importa al inicio del codigo, se crea un objeto de la clase Scanner y se utiliza el metodo nextLine() para leer una linea de texto, nextInt() para leer un entero, nextDouble() para leer un decimal, etc.
+//ciclos for:
+for(int i=0; i<10; i++){
+    System.out.println(i+"");
+}
+
+//ciclo while:
+int i=0;
+while(i<10){
+    System.out.println(i+"");
+    i++;
+}
+
+// ciclo do while:
+do{
+    System.out.println(i+"");
+    i-=200;
+}while (i>=0);
+
+//tipos de variable 
+ //variables tipo string.
+ //declracion de variables.
+ int numero=0;
+ string nombre="Juan";
+ //comparar valores de variables.
+ numero== 9;
+ nombre =="Juan";
+ nombre.equals("Juan"); //para comparar strings se utiliza el metodo equals() en lugar del operador ==, ya que el operador == compara referencias de objetos y no su contenido.
+ 
+ //arreglos: son estructuras de datos que permiten almacenar una colección de elementos del mismo tipo, se declaran con el tipo de dato seguido de corchetes [] y el nombre del arreglo, por ejemplo: int[] numeros; String[] nombres; para inicializar un arreglo se utiliza la palabra reservada new seguida del tipo de dato y el tamaño del arreglo entre corchetes, por ejemplo: numeros = new int[10]; nombres = new String[5]; para acceder a los elementos de un arreglo se utiliza el indice del elemento entre corchetes, por ejemplo: numeros[0] para acceder al primer elemento del arreglo numeros, nombres[1] para acceder al segundo elemento del arreglo nombres.
+ int numeros []= new int[10];
+ 
+ //matrices:
+int numeros [][]= new int[3][3]; //matriz de 3 filas y 3 columnas
+
+for(int j=0;j< filas;j++){
+    for(int i=0;i<columnas;i++){
+        System.out.println("ingrese el numero de a fila"+j " y el valor de la columna"+i);
+        numeros[j][i]= sc.nextInt();
+    }
+}
+
+for(int j=0;j< filas;j++){
+    for(int i=0;i<columnas;i++){
+        numeros[i][j]= contador;
+        contador++;
+        System.out.print("["+numeros[i][j]+"]");
+    }
--- Semestre 2 Java/basesjavaejercicios.java
+++ Semestre 2 Java/basesjavaejercicios.java
@@ -1,65 +1,23 @@
 import java.util.Scanner;
+public class Basesjavaejercicios{
+    public static void main(String args[]){
+        int filas=0;
+        int columnas=0;
+        int contador=1;
+       Scanner sc= new Scanner(System.in);
+System.out.println("ingrese el numero de filas");
+filas= sc.nextInt();
+System.out.println("ingrese el numero de columnas");
+columnas= sc.nextInt();
 
-public class basesjavaejercicios {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num_uno = 0;
-        int num_dos = 0;
-        int resultado = 0;
-        int parametro = 0;
-        
-        System.out.println("Ingrese el numero de la operacion que desea realizar: 1. Suma, 2. Resta, 3. Multiplicacion, 4. Division"); 
-        parametro = scanner.nextInt();
-        
-        switch(parametro){
-            case 1:
-                System.out.println("Realizando la suma de dos numeros");
-                System.out.println("ingrese el primer numero");
-                num_uno = scanner.nextInt();
-                System.out.println("ingrese el segundo numero");
-                num_dos = scanner.nextInt();
-                resultado = num_uno + num_dos;
-                System.out.println("El resultado de la suma es: " + resultado);
-                break;
-
-            case 2:
-                System.out.println("Realizando la resta de dos numeros");
-                System.out.println("ingrese el primer numero");
-                num_uno = scanner.nextInt();
-                System.out.println("ingrese el segundo numero");
-                num_dos = scanner.nextInt();
-                resultado = num_uno - num_dos;
-        
-        
-                System.out.println("El resultado de la resta es: " + resultado);
-                break;
-
-            case 3:
-                System.out.println("Realizando la multiplicacion de dos numeros");
-                System.out.println("ingrese el primer numero");
-                num_uno = scanner.nextInt();
-                System.out.println("ingrese el segundo numero");
-                num_dos = scanner.nextInt();
-                resultado = num_uno * num_dos;
-                System.out.println("El resultado de la multiplicacion es: " + resultado);
-                break;
-            
-            case 4:
-                System.out.println("Realizando la division de dos numeros");
-                System.out.println("ingrese el primer numero");
-                num_uno = scanner.nextInt();
-                System.out.println("ingrese el segundo numero");
-                num_dos = scanner.nextInt();
-                if(num_dos != 0){
-                    resultado = num_uno / num_dos;
-                    System.out.println("El resultado de la division es: " + resultado);
-                } else {
-                    System.out.println("Error: No se puede dividir por cero");
-                }
-                break;
-            
-            default:
-                System.out.println("Opcion no valida");
-        }
+int numeros [][] = new int [filas][columnas];
+for(int i=0;i< filas;i++){
+    for(int j=0;j<columnas;j++){
+        numeros[i][j]= contador;
+        contador++;
+        System.out.print("["+numeros[i][j]+"]");
     }
+    System.out.println("");
+}
+}
 }
