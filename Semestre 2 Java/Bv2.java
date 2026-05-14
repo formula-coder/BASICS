84b17513e091602aacc7089d2eb83de60a128bad
formula-coder <yezidandres2501@gmail.com>
2026-05-14 13:48:44 -0500

    git add

--- Semestre 2 Java/Bv2.java
+++ Semestre 2 Java/Bv2.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Bv2{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Lavadora");
+        System.out.println("Ingrese la cantidad de kg de ropa a lavar");
+        int kg = sc.nextInt();
+
+        System.out.println("Ingrese el tipo de ropa a lavar");
+        System.out.println("1. Ropa blanca");
+        System.out.println("2. Ropa de color");
+        int tiporopa = sc.nextInt();
+
+        Bv1 lavadora = new Bv1(kg, tiporopa);
+        lavadora.ciclofinalizado();
     }
 }
